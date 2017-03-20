602d9c6ec03f9aa30428519121e755af062ac632
Aaron I Trierweiler <aaronitrierweiler@MacBook-Pro.local>
2017-03-20 13:01:53 -0500

    This commit contains new functions, but the add giph button now doesn't work...

--- assets/logic.js
+++ assets/logic.js
@@ -1,5 +1,5 @@
 // The ready event occurs when the DOM (document object model) has been loaded.
-$( document ).ready(function(){
+$(document).ready(function(){
 
 // =========================
 // SETUP VARIABLES
@@ -19,8 +19,11 @@ $( document ).ready(function(){
     "pig"];
 
 
+  // =========================
+  // FUNCTIONS
+  // =========================
   
-  // Function for dumping JSON content for each button into the div and displaying it.
+  // Function for dumping JSON content for each button into the div and displaying gifs when clicked.
   function displayTopicInfo() {
 
     // Adding data attribute with name "data-name" to variable topic
@@ -52,8 +55,25 @@ $( document ).ready(function(){
   }
 
 
+  function gifsClick() {
 
+    $("#gifs-appear-here").on("click", function() {
+      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    var state = $(this).attr("data-state");
+      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+      // Then, set the image's data-state to animate
+      // Else set src to the data-still value
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+    })
   
+  }
+
   // Function for displaying animal data
   function renderButtons() {
 
@@ -79,28 +99,27 @@ $( document ).ready(function(){
   }
 
   
-
-
   // This function handles events where one button is clicked
-  $("#add-topic").on("click", function(event) {
-    // event.preventDefault() prevents the form from trying to submit itself.
-    // We're using a form so that the user can hit enter instead of clicking the button if they want
-    event.preventDefault();
+  function addButton() {
+    
+    $("#add-topic").on("click", function(event) {
+      // event.preventDefault() prevents the form from trying to submit itself.
+      // We're using a form so that the user can hit enter instead of clicking the button if they want
+      event.preventDefault();
 
-    // This line grabs the input from the inputbox
-    // $.val() Get the current value of the first element in the set of matched elements or set the value of every matched element.
-    // $.trim() function removes all newlines, spaces (including non-breaking spaces), and tabs from the beginning and end of the supplied string. If these whitespace characters occur in the middle of the string, they are preserved.
-    var topic = $("#topic-input").val().trim();
+      // This line grabs the input from the inputbox
+      // $.val() Get the current value of the first element in the set of matched elements or set the value of every matched element.
+      // $.trim() function removes all newlines, spaces (including non-breaking spaces), and tabs from the beginning and end of the supplied string. If these whitespace characters occur in the middle of the string, they are preserved.
+      var topic = $("#topic-input").val().trim();
 
-    // Adding the animal from the textbox to our array
-    topics.push(topic);
-    // console.log(topics);
+      // Adding the animal from the textbox to our array
+      topics.push(topic);
+      // console.log(topics);
 
-    // Calling renderButtons which handles the processing of our topics array
-    renderButtons();
-  })
-
-
+      // Calling renderButtons which handles the processing of our topics array
+      renderButtons();
+    })
+  }
 
   // Function for displaying the topic info
   // Using $(document).on instead of $(".topic").on to add event listenersto dynamically generated elements
@@ -109,21 +128,5 @@ $( document ).ready(function(){
   // Calling the renderButtons function to display the intial buttons
   renderButtons();
 
-
-  $("#gifs-appear-here").on("click", function() {
-    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-  var state = $(this).attr("data-state");
-    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-    // Then, set the image's data-state to animate
-    // Else set src to the data-still value
-  if (state === "still") {
-    $(this).attr("src", $(this).attr("data-animate"));
-    $(this).attr("data-state", "animate");
-  } else {
-    $(this).attr("src", $(this).attr("data-still"));
-    $(this).attr("data-state", "still");
-  }
-  
-  })
 
 });
