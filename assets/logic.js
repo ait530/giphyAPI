5da9ede218e231040efd4845f72235c53f2cb722
Aaron I Trierweiler <aaronitrierweiler@MacBook-Pro.local>
2017-03-21 14:16:19 -0500

    good commit

--- assets/logic.js
+++ assets/logic.js
@@ -45,7 +45,7 @@ $(document).ready(function(){
         var animalDiv = $('<div>');
         var p = $('<p>').text("Rating: "+response.data[i].rating);
         var animalImage = $('<img>');
-        animalImage.attr('src',response.data[i].images.fixed_height.url);
+        animalImage.attr('src',response.data[i].images.fixed_height_still.url);
         animalDiv.append(animalImage);
         animalDiv.append(p);
         $("#gifs-appear-here").prepend(animalDiv);
@@ -58,23 +58,23 @@ $(document).ready(function(){
 
 
   
- // $(".animalDiv").on("click", function() {
- //    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
- //    var state = $(this).attr("data-state");
- //    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
- //    // Then, set the image's data-state to animate
- //    // Else set src to the data-still value
- //    if (state === "still") {
- //        $(this).attr("src", $(this).attr("data-animate"));
- //        $(this).attr("data-state", "animate");
- //    } 
- //    else {
- //        $(this).attr("src", $(this).attr("data-still"));
- //        $(this).attr("data-state", "still");
- //      }
- //    })
+  // $("gifs-appear-here").on("click", function() {
+  //   // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+  //   var state = $(this).attr("data-state");
+  //   // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+  //   // Then, set the image's data-state to animate
+  //   // Else set src to the data-still value
+  //   if (state === "still") {
+  //       $(this).attr("src", $(this).attr("data-animate"));
+  //       $(this).attr("data-state", "animate");
+  //   } 
+  //   else {
+  //       $(this).attr("src", $(this).attr("data-still"));
+  //       $(this).attr("data-state", "still");
+  //     }
+  //   })
   
- //  } 
+  // })
 
 
   // This function handles events where one button is clicked
