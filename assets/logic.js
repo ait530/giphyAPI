348a2ff3fc93fe51099e7161d22c4fc77fb082ea
Aaron Trierweiler <ait530@users.noreply.github.com>
2017-03-21 13:19:04 -0500

    gifs render, prepend now allows other buttons to be clicked and render their respective gifs, the add a gif button topic works

--- assets/logic.js
+++ assets/logic.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 // SETUP VARIABLES
 // =========================
 
-// topics variable consisting of an array of strings that consists of animals
+// topics variable equates to an array of strings that consist of animals
   var topics = ["dog",
     "cat",
     "rabbit",
@@ -24,57 +24,81 @@ $(document).ready(function(){
   // =========================
   
   // Function for dumping JSON content for each button into the div and displaying gifs when clicked.
-  function displayTopicInfo() {
+  function gifsClick() {
 
-    // Adding data attribute with name "data-name" to variable topic
-    var topic = $(this).attr("data-name");
+    // Setting attribute with name "data-name" to variable topics
+    var topics = $(this).attr("data-name");
 
     // Giphy URL + string from array + the api key
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
-      topic + "&api_key=dc6zaTOxFJmzC&limit=10&rating";
+      topics + "&api_key=dc6zaTOxFJmzC&limit=10&rating";
    
-    // Creating an AJAX call for the specific animal button being clicked
+    // Creating an AJAX (asynchronous HTTP request) calls the giphy API for the specific animal button being clicked
     $.ajax({
       url: queryURL,
       method: "GET"
     }).done(function(response) {
-
+      // Server sends data back to browser, browser returns data using JavaScript.
+      // For each index in topics array, set an animalDIV, p element, and a animal image div. For the animal image, set the src to the data response from the server for each animal and give the images a fixed height.
+      //Append: Insert content, specified by the parameter, to the end of each element in the set of matched elements. In this case, append the p element and animalImage to the animalDiv. Then, insert this content to the desired HTML div for the gifs to appear, thus #gifs-appear-here. 
       for (var i = 0; i < response.data.length; i++) {
         var animalDiv = $('<div>');
         var p = $('<p>').text("Rating: "+response.data[i].rating);
         var animalImage = $('<img>');
         animalImage.attr('src',response.data[i].images.fixed_height.url);
-        animalDiv.append(p);
         animalDiv.append(animalImage);
-        $("#gifs-appear-here").append(animalDiv);
+        animalDiv.append(p);
+        $("#gifs-appear-here").prepend(animalDiv);
         // $("#gifs-appear-here").html(JSON.stringify(response.data));
         // renderButtons();
       }
     })
-  // end of diplayTopicInfo function
+  // end of gifsClick function
   }
 
 
-  function gifsClick() {
-
-    $("#gifs-appear-here").on("click", function() {
-      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-    var state = $(this).attr("data-state");
-      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-      // Then, set the image's data-state to animate
-      // Else set src to the data-still value
-      if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-      } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-      }
-    })
   
-  }
+ // $(".animalDiv").on("click", function() {
+ //    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+ //    var state = $(this).attr("data-state");
+ //    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+ //    // Then, set the image's data-state to animate
+ //    // Else set src to the data-still value
+ //    if (state === "still") {
+ //        $(this).attr("src", $(this).attr("data-animate"));
+ //        $(this).attr("data-state", "animate");
+ //    } 
+ //    else {
+ //        $(this).attr("src", $(this).attr("data-still"));
+ //        $(this).attr("data-state", "still");
+ //      }
+ //    })
+  
+ //  } 
 
-  // Function for displaying animal data
+
+  // This function handles events where one button is clicked
+  $("#add-topic").on("click", function(event) {
+    // event.preventDefault() prevents the form from trying to submit itself.
+    // We're using a form so that the user can hit enter instead of clicking the button if they want
+    event.preventDefault();
+    // This line grabs the input from the inputbox
+    // $.val() Get the current value of the first element in the set of matched elements or set the value of every matched element.
+    // $.trim() function removes all newlines, spaces (including non-breaking spaces), and tabs from the beginning and end of the supplied string. If these whitespace characters occur in the middle of the string, they are preserved.
+    var topic = $("#topic-input").val().trim();
+
+    // Adding the animal from the textbox to our array
+    topics.push(topic);
+    // console.log(topics);
+
+    // Calling renderButtons which handles the processing of our topics array
+    renderButtons();
+  })
+
+
+
+
+  // Function for displaying new animal data
   function renderButtons() {
 
     // Deleting the buttons prior to adding new topics
@@ -99,31 +123,10 @@ $(document).ready(function(){
   }
 
   
-  // This function handles events where one button is clicked
-  function addButton() {
-    
-    $("#add-topic").on("click", function(event) {
-      // event.preventDefault() prevents the form from trying to submit itself.
-      // We're using a form so that the user can hit enter instead of clicking the button if they want
-      event.preventDefault();
-
-      // This line grabs the input from the inputbox
-      // $.val() Get the current value of the first element in the set of matched elements or set the value of every matched element.
-      // $.trim() function removes all newlines, spaces (including non-breaking spaces), and tabs from the beginning and end of the supplied string. If these whitespace characters occur in the middle of the string, they are preserved.
-      var topic = $("#topic-input").val().trim();
-
-      // Adding the animal from the textbox to our array
-      topics.push(topic);
-      // console.log(topics);
-
-      // Calling renderButtons which handles the processing of our topics array
-      renderButtons();
-    })
-  }
-
+ 
   // Function for displaying the topic info
   // Using $(document).on instead of $(".topic").on to add event listenersto dynamically generated elements
-  $(document).on("click", ".topic", displayTopicInfo);
+  $(document).on("click", ".topic", gifsClick);
 
   // Calling the renderButtons function to display the intial buttons
   renderButtons();
